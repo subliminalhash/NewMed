2b584c8a9cee1e5018a5df1aec5b89c78c359643
SubliminalHash <kemalemin@gmail.com>
2022-08-12 20:25:27 +0300

    order-list html , css, js Release Candidate

--- src/js/orders-list.js
+++ src/js/orders-list.js
@@ -4,12 +4,23 @@
 // });
 
 const search = document.getElementById("q");
+const searchddl = document.getElementById("divSearchSuggestionsWrapper");
 
 search.addEventListener("focus", function (e) {
-  const searchddl = document.getElementById("divSearchSuggestionsWrapper");
   const inputTop = search.offsetTop;
   const inputHeight = search.offsetHeight;
-  searchddl.style.top = inputTop + inputHeight;
+  searchddl.style.top = inputTop + inputHeight - 10;
 
   searchddl.classList.toggle("d-none");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.id === "q") return;
+
+  const withinBoundaries = e.composedPath().includes(searchddl);
+
+  if (!withinBoundaries && !searchddl.classList.contains("d-none")) {
+    console.log("both are false so hiding the menu");
+    searchddl.classList.add("d-none");
+  }
 });
