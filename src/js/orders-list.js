de66174752c7fcb12a666606212e6d50318ff62e
SubliminalHash <kemalemin@gmail.com>
2022-08-12 21:00:48 +0300

    orders-list RC1 more tweaks

--- src/js/orders-list.js
+++ src/js/orders-list.js
@@ -7,10 +7,10 @@ const search = document.getElementById("q");
 const searchddl = document.getElementById("divSearchSuggestionsWrapper");
 
 search.addEventListener("focus", function (e) {
-  const inputTop = search.offsetTop;
+  document.getElementById("divSearch").classList.add("show");
+  const inputTop = search.offsetTop - 5;
   const inputHeight = search.offsetHeight;
-  searchddl.style.top = inputTop + inputHeight - 10;
-
+  searchddl.style.top = inputTop + inputHeight;
   searchddl.classList.toggle("d-none");
 });
 
@@ -22,5 +22,6 @@ window.addEventListener("click", (e) => {
   if (!withinBoundaries && !searchddl.classList.contains("d-none")) {
     console.log("both are false so hiding the menu");
     searchddl.classList.add("d-none");
+    document.getElementById("divSearch").classList.remove("show");
   }
 });
